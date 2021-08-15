4c590f1558436c1363cbf4ae3eadb7213819f501
gideongurning31 <gideon.gurning@gmail.com>
2021-08-15 22:36:25 +0700

    Create initial meals by category FlatList

--- screens/CategoryMeals.js
+++ screens/CategoryMeals.js
@@ -1,16 +1,27 @@
 import React from 'react';
-import { StyleSheet, View, Image } from 'react-native';
+import { StyleSheet, View, Image, FlatList } from 'react-native';
 import globalStyles from '../global/Styles';
 import TextBold from '../widgets/TextBold';
+import TextReg from '../widgets/TextReg';
+import { MEALS } from '../models/MealsModel';
 
 export default ({ route: { params } }) => {
+  const mealsByCategory = MEALS.filter(meal => meal.categoryIds.indexOf(params.id) > -1);
+
   return (
     <View style={{ ...globalStyles.container, paddingTop: 15 }}>
       <Image style={styles.image} source={params.imagePath} />
       <TextBold style={styles.title}>
         {params.title.toUpperCase()} {['c1', 'c2', 'c3', 'c4'].indexOf(params.id) > -1 && 'FOOD'}
       </TextBold>
-      <View style={styles.content}></View>
+      <FlatList
+        data={mealsByCategory}
+        renderItem={({ item }) => (
+          <View>
+            <TextReg>{item.title}</TextReg>
+          </View>
+        )}
+      />
     </View>
   );
 };
